f37f0fe532150496557619ad7025aaa4ae065fd9
rafahg <r.hgar78@gmail.com>
2020-12-13 20:26:19 +0000

    day 13 solution part1

--- Day_13/solution.rb
+++ Day_13/solution.rb
@@ -1,3 +1,19 @@
 INPUT = File.readlines('./Day_13/data.txt', chomp:true)
-p time = INPUT.first
-p bus_lines = INPUT.last.delete!('x').scan(/\d+/)
+time = INPUT.first.to_i
+
+bus_lines = INPUT.last.delete!('x').scan(/\d+/).map(&:to_i).sort
+
+time_line = (time..time + 20).to_a
+result = []
+
+ while result.first == nil
+  time_line.each do |minute|
+    bus_lines.each do |bus|
+      if minute % bus == 0
+        result << [minute,bus]
+      end
+    end
+  end
+ end
+
+p waiting_time = (result.first.first - time) * result.first[1]
